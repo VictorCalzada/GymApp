86850c7a2ed6ea66019410cbf21515d5311981fb
Victor Calzada <victor.calzada.cabano@gmail.com>
2021-10-08 19:17:13 +0200

    Usuario creado

---
+++ src/usuario.py
@@ -0,0 +1,11 @@
+class Usuario(object):
+    """
+    Author: Victor Calzada
+    Date: 08/10/2021
+
+    Clase usuario para manejar los ejercicios que ejecuta
+    """
+
+
+    def __init__(self, str: nombre):
+        self.nombre = nombre
